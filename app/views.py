2e1768ef5a37bfaeba9bb2543ad85d6ade62fae4
Yagmur <yagmur.eff1@gmail.com>
2024-08-14 12:19:49 -0500

    Jinja template and render view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("hello world")
+    return render(request, "home.html")
 
